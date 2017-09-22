cf76916db839500d284784a97e3aca5557b5c3f9
Justman <fizvlad@bk.ru>
2017-09-22 07:21:36 +0300

    Init

---
+++ 2nd course/HW_2_Graph/graph.h
@@ -0,0 +1,16 @@
+#ifndef GRAPH_H_INCLUDED
+#define GRAPH_H_INCLUDED
+
+template <typename __T> class _Graph_node
+{
+    friend class
+};
+
+template <typename _T> class Graph
+{
+public:
+private:
+
+};
+
+#endif // GRAPH_H_INCLUDED
