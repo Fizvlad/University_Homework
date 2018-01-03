2845b6858a4d0e14394ce392fe081f44249cecce
Fizvlad <fizvlad@mail.ru>
2018-01-03 21:01:24 +0300

    Small fix

--- Project_VKBot/tests/api_requests/main.cpp
+++ Project_VKBot/tests/api_requests/main.cpp
@@ -2,11 +2,12 @@
 #include "../../include/VK_API.h"
 
 using namespace std;
+using json = nlohmann::json;
 
 int main()
 {
     try {
-        string buffer = vk_api::apiRequest("groups.getMembers", "group_id=157926798"); // Token not needed
+        json buffer = vk_api::apiRequest("groups.getMembers", "group_id=157926798"); // Token not needed
         cout << buffer;
     }
     catch (exception e) {
