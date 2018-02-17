4296eccb7dee0c93e18bb470ad48ff1d58ec2133
Fizvlad <fizvlad@mail.ru>
2018-02-17 21:18:45 +0300

    Fix. Started working on task solution

--- Computational Mathematics homework/Homework #1/HW_1/main.cpp
+++ Computational Mathematics homework/Homework #1/HW_1/main.cpp
@@ -7,13 +7,13 @@ double task_3_function (double x) {
     return x - (4.0/3.0) * log(x) - (5.0/3.0);
 }
 double task_3_function_derivative (double x) {
-    return x - (4.0/3.0) / x;
+    return 1.0 - (4.0/3.0) / x;
 }
 
 int main()
 {
     Function <Consumer_Function_t> task_3(task_3_function, task_3_function_derivative);
-    Interval interval = findDrawdown(task_3, Interval(1e-8, 10));
-    cout << interval << endl;
+    Point p = findSolution(task_3, Interval(1e-10, 10));
+    cout << p << endl;
     return 0;
 }
