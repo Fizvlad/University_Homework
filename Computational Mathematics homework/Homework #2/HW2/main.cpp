e7a18e21710579c98d32c89b3aca15d35eaa31e9
Fizvlad <fizvlad@mail.ru>
2018-03-18 23:26:58 +0300

    Hermite interpolation

--- Computational Mathematics homework/Homework #2/HW2/main.cpp
+++ Computational Mathematics homework/Homework #2/HW2/main.cpp
@@ -7,28 +7,58 @@ using namespace interpolation;
 
 int main()
 {
+    cout.precision(8);
     // Task 1
-    std::vector<Point> points({
-    {0.05, 20},
-    {0.1, 10},
-    {0.15, 6.6666},
-    {0.2, 5},
-    {0.25, 4},
-    {0.3, 3.3333},
-    {0.35, 2.8571},
-    {0.4, 2.5},
-    {0.45, 2.2222},
-    {0.5, 2},
-    });
-
-    Polynomial polynomial = interpolate(points);
-    cout << "Interpolation result:    " << polynomial << endl << endl;
-    for (Point p : points) {
-        cout << "f(" << p.x << ") = " << polynomial(p.x) << "; Correct value: " << p.y << endl;
+    vector<InterpolationPoint> points1 = {
+        InterpolationPoint(0.05, 20),
+        InterpolationPoint(0.1, 10),
+        InterpolationPoint(0.15, 6.6666),
+        InterpolationPoint(0.2, 5),
+        InterpolationPoint(0.25, 4),
+        InterpolationPoint(0.3, 3.3333),
+        InterpolationPoint(0.35, 2.8571),
+        InterpolationPoint(0.4, 2.5),
+        InterpolationPoint(0.45, 2.2222),
+        InterpolationPoint(0.5, 2)
+    };
+    Polynomial polynomial1 = interpolate(points1);
+    cout << "Interpolation result:    " << polynomial1 << endl << endl;
+    for (InterpolationPoint p : points1) {
+        cout << "f(" << p.x << ") = " << polynomial1(p.x) << "; Correct value: " << p.f[0] << endl;
     }
     cout << endl;
-    cout << "y(0.28) = " << polynomial(0.28) << endl;
-    cout << "y(0.475) = " << polynomial(0.475) << endl;
+    cout << "y(0.28) = " << polynomial1(0.28) << endl;
+    cout << "y(0.475) = " << polynomial1(0.475) << endl;
+
+    cout << endl << endl;
+    // Task 2
+    vector<InterpolationPoint> points2 = {
+        InterpolationPoint(0, {1, 2, 10, 36, 192}),
+        InterpolationPoint(1, {36, 144}),
+        InterpolationPoint(2, {1225, 3710})
+    };
+
+    size_t max_r = 5;
+    std::vector<Polynomial> polynomials2(max_r);
+    polynomials2[0] = interpolate(points2);
+    for (size_t i = 0; i < max_r - 1; i++) {
+        Polynomial d = derivative(polynomials2[i]);
+        polynomials2[i + 1] = d;
+    }
+    cout << "Interpolation result:    ";
+    for (Polynomial d : polynomials2) {
+        cout << d << endl;
+    }
+    cout << endl;
+    for (InterpolationPoint p : points2) {
+        for (size_t j = 0; j < p.f.size() && j < polynomials2.size(); j++) {
+            cout << "f";
+            for (size_t k = 0; k < j; k++) {
+                cout << "'";
+            }
+            cout << "(" << p.x << ") = " << polynomials2[j](p.x) << "; Correct value: " << p.f[j] << endl;
+        }
+    }
 
     return 0;
 }
